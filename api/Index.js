730aeaa0a499de12093a6257509f8f5be29be251
Yunus Emre Uyanık <y.emreuyanik@gmail.com>
2023-12-01 16:36:32 +0300

    Update Index.js

--- api/Index.js
+++ api/Index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://blog-mern-client.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next(););
+  next());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
