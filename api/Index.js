d0f94ed2f0217eac493165166b8add31998de641
Yunus Emre Uyanık <y.emreuyanik@gmail.com>
2023-12-01 16:13:04 +0300

    Update Index.js

--- api/Index.js
+++ api/Index.js
@@ -16,7 +16,7 @@ const fs = require("fs");
 const salt = bcrypt.genSaltSync(10);
 const secret = "asdljlkdasjadsjkdasdasdasdasdasda";
 
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({ credentials: true, origin: "https://blog-mern-client.vercel.app" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
