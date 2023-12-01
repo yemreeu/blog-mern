a57563e36b421e6f40f6cad5e288c8ceb6a14ac9
Yunus Emre Uyanık <y.emreuyanik@gmail.com>
2023-12-01 16:36:20 +0300

    Update Index.js

--- api/Index.js
+++ api/Index.js
@@ -16,7 +16,11 @@ const fs = require("fs");
 const salt = bcrypt.genSaltSync(10);
 const secret = "asdljlkdasjadsjkdasdasdasdasdasda";
 
-app.use(cors({ credentials: true, origin: "https://blog-mern-client.vercel.app" }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://blog-mern-client.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next(););
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
